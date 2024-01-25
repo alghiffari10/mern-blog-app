ad733e5353a537c4af0445efe47fcafdbc8fb82e
alghiffari10 <raihanalghiffary28@gmail.com>
2024-01-25 13:45:33 +0700

    add functionality to the sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,36 @@
-import { Link } from 'react-router-dom';
-import { Button, Label, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import axios from 'axios';
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChance = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password)
+      return setErrorMessage('Please fill all fields');
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await axios.post('/api/auth/signup', formData);
+      setLoading(false);
+      if (response.status === 200) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="min-h-screen mt-20">
+    <div className="min-h-screen mt-20 dark">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
@@ -19,23 +47,46 @@ export default function SignUp() {
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your username" />
-              <TextInput id="username" type="text" placeholder="Username" />
+              <Label value="Your username" className="dark:text-black" />
+              <TextInput
+                id="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChance}
+              />
             </div>
             <div>
-              <Label value="Your email" />
-              <TextInput id="email" type="email" placeholder="name@company.com" />
+              <Label value="Your email" className="dark:text-black" />
+              <TextInput
+                id="email"
+                type="email"
+                placeholder="name@company.com"
+                onChange={handleChance}
+              />
             </div>
             <div>
-              <Label value="Your password" />
-              <TextInput id="password" type="password" placeholder="Password" />
+              <Label value="Your password" className="dark:text-black" />
+              <TextInput
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChance}
+              />
             </div>
             <Button
               className="bg-gradient-to-r from-gray-700 via-gray-900 to-black hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 dark:focus:ring-gray-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
-              type="submit">
-              Sign Up
+              type="submit"
+              disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size="sm" light={true} />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -44,6 +95,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5  md:absolute md:w-[362px] " color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
